3cd5268e7cd46517b2dca12d45ad613b250d86a3
Lachlan Campbell <35567774+Doxxus@users.noreply.github.com>
2021-03-04 16:20:16 -0500

    Created event handler class

---
+++ ClientApp/ServerApp/RequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ServerApp {
+    class RequestHandler {
+        public byte[] handleIncomingRequest(string req) {
+            Console.WriteLine("Recieved Request: " + req);
+
+            try {
+                ClientInfo user = JsonConvert.DeserializeObject<ClientInfo>(req);
+                return System.Text.Encoding.Unicode.GetBytes(user.fname + " " + user.lname + " logged successfully.");
+            } catch (Exception e) {
+                return System.Text.Encoding.Unicode.GetBytes(e.ToString());
+            }
+        }
+
+    }
+}
--- ClientApp/ServerApp/Server.cs
+++ ClientApp/ServerApp/Server.cs
@@ -6,18 +6,27 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using System.Net.Mail;
-using Newtonsoft.Json;
 
 namespace ServerApp {
     class Server {
         IPEndPoint ep;
         TcpListener listener;
+        RequestHandler handler;
+
+        byte[] request;
+        byte[] response;
+
+        const int bytesize = 1024 * 1024;
+
 
         public void init() {
             Console.WriteLine("Starting Server...");
 
             ep = new IPEndPoint(IPAddress.Loopback, 3000);
             listener = new TcpListener(ep);
+
+            request = new byte[bytesize];
+
             listener.Start();
 
             Console.WriteLine(@"Listening for requests at: {0}:{1}",
@@ -26,27 +35,16 @@ namespace ServerApp {
 
         public void run() {
             while (true) {
-                const int bytesize = 1024 * 1024;
-
-                string message = null;
-                byte[] buffer = new byte[bytesize];
-
                 var sender = listener.AcceptTcpClient();
-                sender.GetStream().Read(buffer, 0, bytesize);
+                sender.GetStream().Read(request, 0, bytesize);
 
-                message = cleanMessage(buffer);
+                response = handler.handleIncomingRequest(cleanMessage(request));
 
-                Console.WriteLine("Recieved Message: " + message);
-
-                ClientInfo user = JsonConvert.DeserializeObject<ClientInfo>(message);
-
-                byte[] bytes = System.Text.Encoding.Unicode.GetBytes(user.fname + " " + user.lname + " logged successfully.");
-                sender.GetStream().Write(bytes, 0, bytes.Length); 
+                sender.GetStream().Write(response, 0, response.Length); 
             }
         }
 
         private static string cleanMessage(byte[] bytes) {
-            Console.WriteLine("test");
             string message = System.Text.Encoding.Unicode.GetString(bytes);
 
             string result = null;
